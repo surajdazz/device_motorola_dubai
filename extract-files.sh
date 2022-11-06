41cfaa8497aafebcdd19590ec6fc5fd73006fa1d
sb6596 <shubhamprince111@gmail.com>
2022-11-06 09:46:06 +0000

    dubai: Import prebuilt Moto Dolby
    Decommonized from sm7325-common
    Also updated MotoDolbyDax3 from dubai 13 while we're at it.
    
    Ref:
    https://github.com/ArrowOS-Devices/android_device_motorola_sm7325-common/commit/edbad4deb21accdeefb8f7b944a2fb365868aecb
    https://github.com/ArrowOS-Devices/android_device_motorola_sm7325-common/commit/eeaa7dac4b3a8c93cd7aa4551915e451cbfe30a3
    
    Change-Id: I03be92971bd5e40610265ecf531b5a43e0dd0438
    
    dubai: property: Load libstagefrightdolby for dolby codecs
    
    Requires:
    Dolby blobs +
    crdroidandroid/android_frameworks_av@beacf34
    crdroidandroid/android_frameworks_av@8a73000
    crdroidandroid/android_device_crdroid_sepolicy@d7828eb
    
    dubai: audio: Add missing dolby effects
    Co-Authored-By: Pranav Vashi <1227398+neobuddy89@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,10 @@
 
 function blob_fixup() {
     case "${1}" in
+    case "${1}" in
+        vendor/etc/dolby/dax-default.xml)
+            sed -i 's|volume-leveler-enable value="true"|volume-leveler-enable value="false"|g' "${2}"
+            ;;
         vendor/lib/libmot_chi_desktop_helper.so | vendor/lib64/libmot_chi_desktop_helper.so)
             grep -q "libgui_shim_vendor.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim_vendor.so" "${2}"
             ;;
