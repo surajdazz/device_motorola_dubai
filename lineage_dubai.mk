7dfc3b1cf44fad40e8889db521ba9537186ba669
Suraj Das <surajdazz@gmail.com>
2023-03-14 11:26:13 +0530

    Riced

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -14,14 +14,6 @@ $(call inherit-product, device/motorola/dubai/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Custom features
-TARGET_HAS_UDFPS := true
-TARGET_ENABLE_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-
-# Inherit Gapps
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-
 PRODUCT_NAME := lineage_dubai
 PRODUCT_DEVICE := dubai
 PRODUCT_MANUFACTURER := motorola
@@ -35,3 +27,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dubai_g-user 12 S1RD32.55-106 0cdff5-e7305 release-keys"
 
 BUILD_FINGERPRINT := motorola/dubai_g/dubai:12/S1RD32.55-106/0cdff5-e7305:user/release-keys
+
+# riceDroid Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+RICE_CHIPSET := SD778G+
+RICE_MAINTAINER := Suraj.Das
+WITH_GMS := true
+SUSHI_BOOTANIMATION := 1440
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+TARGET_USE_PIXEL_FINGERPRINT := false
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_USE_GOOGLE_TELEPHONY := false
